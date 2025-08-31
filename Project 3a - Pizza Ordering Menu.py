99d8e737ba8a8bb45fcba0c3c24fc0f189d6c824
Opal R <opalratan@gmail.com>
2025-08-31 19:11:25 -0400

    Create Project 3a - Pizza Ordering Menu.py

---
+++ Project 3a - Pizza Ordering Menu.py
@@ -0,0 +1,47 @@
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M or L: ")
+pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
+extra_cheese = input("Do you want extra cheese? Y or N: ")
+total_price = 0
+if size == "S":
+    total_price += 15
+    if pepperoni == "Y":
+        total_price += 2
+        if extra_cheese == "Y":
+            total_price += 1
+            print("Your total is $" + str(total_price))
+        else:
+            print("Your total is $" + str(total_price))
+    elif extra_cheese == "Y":
+        total_price += 1
+        print("Your total is $" + str(total_price))
+    else:
+        print("Your total is $" + str(total_price))
+elif size == "M":
+    total_price += 20
+    if pepperoni == "Y":
+        total_price += 3
+        if extra_cheese == "Y":
+            total_price += 1
+            print("Your total is $" + str(total_price))
+        else:
+            print("Your total is $" + str(total_price))
+    elif extra_cheese == "Y":
+        total_price += 1
+        print("Your total is $" + str(total_price))
+    else:
+        print("Your total is $" + str(total_price))
+else:
+    total_price += 25
+    if pepperoni == "Y":
+        total_price += 3
+        if extra_cheese == "Y":
+            total_price += 1
+            print("Your total is $" + str(total_price))
+        else:
+            print("Your total is $" + str(total_price))
+    elif extra_cheese == "Y":
+        total_price += 1
+        print("Your total is $" + str(total_price))
+    else:
+        print("Your total is $" + str(total_price))
