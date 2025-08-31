3766a6eed08056724aa90d217ec1c12f5eb8cec3
Opal R <opalratan@gmail.com>
2025-08-31 19:10:40 -0400

    Create Project 1 - Band Name Generator.py

---
+++ Project 1 - Band Name Generator.py
@@ -0,0 +1,4 @@
+print("Hello, welcome to the Band Name Generator") # Prints the message
+city_name = input("What city did you grow up in?\n") # Asks for input for city and stores in city_name variable
+pet_name = input("What was the name of your pet?\n") # Asks for input for pet name and stores in pet_name variable
+print("Congrats! Your band name is:" + city_name + " " + pet_name) # Prints message and concatenates the city_name & pet_name variable then outputs.
